dbf5f4e42f49b18916fbe5c38a16764af42c1e1d
XiongYL <xiong21@fmmu.edu.cn>
2024-02-19 17:47:23 +0800

    commit

---
+++ R/HSurADs.R
@@ -0,0 +1,10 @@
+#'HSurADs
+#'
+#'HSurADs is used for predicting the survival rate of lung adenocarcinoma patients
+#'
+#'@section HpSurADs
+#'Functions for lung adenocarcinoma
+#'@docType package
+#'@name HSurADs
+
+NULL
